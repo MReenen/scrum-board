b20c9d1bd813976b0f982524b3fa7c233c01ce57
Mats van Reenen <mail@matsvanreenen.nl>
2020-12-31 18:22:54 +0100

    improve test workfow

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,15 @@
  * scrum-board
  *
  * @category   scrum-board
- * @author     Vaibhav Mehta <vaibhav@decodingweb.com>
- * @copyright  Copyright (c) 2016 Vaibhav Mehta <https://github.com/i-break-codes>
+ * @author     Vaibhav Mehta <vaibhav@decodingweb.com>, Mats van Reenen <mail@matsvanreenen.nl>
+ * @copyright  Copyright (c) 2016,2020 Vaibhav Mehta <https://github.com/i-break-codes>
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  * @version    1.0 Beta
  */
 
-const Gulp = require('gulp');
 const BrowserSync  = require('browser-sync');
+const Gulp = require('gulp');
+Gulp.clean = require('gulp-clean');
 const sass         = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS     = require('gulp-clean-css');
@@ -46,7 +47,7 @@ function css(){
     .pipe(Gulp.dest('app/assets/stylesheets'))
 }
 function cssMinify(){
-  return Gulp.src('app/assets/stylesheets/**/*.css')
+  return Gulp.src(['app/assets/stylesheets/**/*.css', '!app/assets/stylesheets/**/*.min.css'])
     .pipe(cleanCSS({debug: true}, function(details) {
       console.log('Original Size : ' + details.name + ': ' + details.stats.originalSize + ' bytes');
       console.log('Minified Size : ' + details.name + ': ' + details.stats.minifiedSize + ' bytes');
@@ -59,7 +60,7 @@ function cssMinify(){
 };
 
 function jsMinify(){
-  return Gulp.src('src/javascript/**/*.js')
+  return Gulp.src('src/scripts/**/*.js')
     .pipe(uglify())
     .pipe(renameFiles({ suffix: '.min' }))
     .pipe(Gulp.dest('app/assets/scripts'))
@@ -69,12 +70,43 @@ function jsMinify(){
     // }));
 };
 
-const cssTask = Gulp.series(css, cssMinify);
-const jsTask = jsMinify
+function clean(mode = 'all'){
+  const opts = {read: false, allowEmpty: true}
+  switch(mode){
+    case 'cssTmp':
+      return function cleanTmpCSS(){
+        return Gulp.src(['app/assets/stylesheets/**/*.css', '!app/assets/stylesheets/**/*.min.css'], opts)
+          .pipe(Gulp.clean())
+      }
+    case 'tmp':
+      return clean('cssTmp');
+
+    case 'css':
+      return function cleanCSS(){
+        return Gulp.src(['app/assets/stylesheets'], opts)
+          .pipe(Gulp.clean())
+      }
+    case 'js':
+      return function cleanJS(){
+        return Gulp.src(['app/assets/scripts'], opts)
+          .pipe(Gulp.clean())
+      }
+    case 'all':
+      return Gulp.parallel(clean('css'), clean('js'));
+    default:
+      console.log(`WARNING: Clean: invalid mode '${mode}'`)
+      console.log(`dong 'tmp' instad of '${mode}'`)
+      return clean('tmp')
+  }
+}
+
+const cssTask = Gulp.series(css, cssMinify, clean('cssTmp'));
+const jsTask = jsMinify;
 const buildTask = Gulp.parallel(cssTask, jsTask);
 
+exports.clean = clean('all');
 exports.server = browserSync;
 exports.cssBuild = cssTask;
 exports.jsBuild = jsTask;
-exports.build = buildTask;
+exports.build = Gulp.series(clean('all'), buildTask);
 exports.default = Gulp.series(buildTask, browserSync);
